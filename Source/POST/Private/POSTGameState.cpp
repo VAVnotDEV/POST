575714c77568f72cbbdb1c9fb5e087c3ed34d072
Veselkov Andrew <D:\Mail>
2026-05-12 17:01:26 +0500

    Add GameState class

---
+++ Source/POST/Private/POSTGameState.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2026 VAVnotDev. All Rights Reserved.
+
+
+#include "POSTGameState.h"
+
+APOSTGameState::APOSTGameState()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+bool APOSTGameState::IsNight() const
+{
+	return Hours >= 22 || Hours < 6;
+}
+
+void APOSTGameState::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AccumulateRealSeconds += DeltaTime * TimeScale;
+
+	while (AccumulateRealSeconds >= 1.0f)
+	{
+		AccumulateRealSeconds -= 1.0f;
+		AddGameSecond();
+	}
+
+}
+
+void APOSTGameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	bIsNight = IsNight();
+	CheckDayNightState();
+}
+
+void APOSTGameState::AddGameSecond()
+{
+	Seconds++;
+
+	if (Seconds >= 60)
+	{
+		Seconds = 0;
+		Minutes++;
+
+		OnMinuteChanged.Broadcast(Minutes);
+	}
+
+	if (Minutes >= 60)
+	{
+		Minutes = 0;
+		Hours++;
+
+		if (Hours >= 24)
+			Hours = 0;
+
+		OnHourChanged.Broadcast(Hours);
+		CheckDayNightState();
+	}
+
+
+}
+
+void APOSTGameState::CheckDayNightState()
+{
+	const bool bNowNight = IsNight();
+}
---
+++ Source/POST/Public/POSTGameState.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2026 VAVnotDev. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "POSTGameState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHourChanged, int32, NewHour);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMinuteChanged, int, NewMinute);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNightStarted);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDayStarted);
+
+UCLASS()
+class POST_API APOSTGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	APOSTGameState();
+
+	UPROPERTY(BlueprintAssignable, Category = "POST Time")
+	FOnHourChanged OnHourChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "POST Time")
+	FOnMinuteChanged OnMinuteChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "POST Time")
+	FOnNightStarted OnNightStarted;
+
+	UPROPERTY(BlueprintAssignable, Category = "POST Time")
+	FOnDayStarted OnDayStarted;
+
+	int32 GetHours() const { return Hours; }
+	int32 GetMinutes() const { return Minutes; }
+	int32 GetSeconds() const { return Seconds; }
+
+	bool IsNight() const;
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "POST Time")
+	int32 Hours = 21;
+
+	UPROPERTY(EditAnywhere, Category = "POST Time")
+	int32 Minutes = 0;
+
+	UPROPERTY(EditAnywhere, Category = "POST Time")
+	int32 Seconds = 0;
+
+	UPROPERTY(EditAnywhere, Category = "POSt Time")
+	float TimeScale = 60.0f;
+
+	float AccumulateRealSeconds = 0.0f;
+
+	bool bIsNight = false;
+
+	void AddGameSecond();
+	void CheckDayNightState();
+};
