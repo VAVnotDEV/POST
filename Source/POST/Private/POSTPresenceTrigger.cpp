2512eb2c1dcc9f67eb1cf75ecfbb4a83d7dac5ab
VAV <veselyi93rus@gmail.com>
2026-09-13 17:30:50 +0500

    Edit PresenceTrigger class

--- Source/POST/Private/POSTPresenceTrigger.cpp
+++ Source/POST/Private/POSTPresenceTrigger.cpp
@@ -6,14 +6,16 @@
 #include "Gameplay/POSTGameDirector.h"
 #include "Kismet/GameplayStatics.h"
 #include "Player/POSTCharacter.h"	
+#include "POSTLog.h"
 
 // Sets default values
 APOSTPresenceTrigger::APOSTPresenceTrigger()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
 	SetRootComponent(TriggerBox);
+	TriggerBox->SetGenerateOverlapEvents(true);
 	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
 	TriggerBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
@@ -22,10 +24,18 @@ APOSTPresenceTrigger::APOSTPresenceTrigger()
 UFUNCTION()
 void APOSTPresenceTrigger::OnTriggeBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!Director) return;
+	UE_LOG(LogPOST, Warning, TEXT("TRIGGER FIRED: %s"), *GetNameSafe(OtherActor));
+	if (!Director)
+	{
+		UE_LOG(LogPOST, Error, TEXT("Presence Director NOT FOUND"));
+		return;
+	}
 
 	if (Cast<APOSTCharacter>(OtherActor))
+	{
+		UE_LOG(LogPOST, Warning, TEXT("Presence: Player entered trigger"));
 		Director->StartPresenceEncounter();
+	}
 }
 
 UFUNCTION()
@@ -33,8 +43,11 @@ void APOSTPresenceTrigger::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedCo
 {
 	if (!Director) return;
 
-	if (Cast<APOSTGameDirector>(OtherActor))
+	if (Cast<APOSTCharacter>(OtherActor))
+	{
+		UE_LOG(LogPOST, Warning, TEXT("Presence: Player left trigger"));
 		Director->StopPresenceEncounter();
+	}
 }
 
 // Called when the game starts or when spawned
@@ -42,7 +55,6 @@ void APOSTPresenceTrigger::BeginPlay()
 {
 	Director = Cast<APOSTGameDirector>(UGameplayStatics::GetActorOfClass(this, APOSTGameDirector::StaticClass()));
 	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &APOSTPresenceTrigger::OnTriggeBeginOverlap);
-
 	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &APOSTPresenceTrigger::OnTriggerEndOverlap);
 
 
--- Source/POST/Public/POSTPresenceTrigger.h
+++ Source/POST/Public/POSTPresenceTrigger.h
@@ -24,7 +24,9 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	UBoxComponent* TriggerBox;
 
+	UFUNCTION()
 	void OnTriggeBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
 	void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	virtual void BeginPlay() override;
 	
