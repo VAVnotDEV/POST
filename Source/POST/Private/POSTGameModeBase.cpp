dde4fc16c1d5a9ab6183dbd2aa0f493be72e4382
VAVnotDEV <163330007+VAVnotDEV@users.noreply.github.com>
2026-09-17 15:05:14 +0500

    Ensure cycle manager exists in gameplay levels

--- Source/POST/Private/POSTGameModeBase.cpp
+++ Source/POST/Private/POSTGameModeBase.cpp
@@ -1,7 +1,9 @@
 // Copyright (c) 2026 VAVnotDev. All Rights Reserved.
 
-
 #include "POSTGameModeBase.h"
+
+#include "Gameplay/POSTCycleManager.h"
+#include "Kismet/GameplayStatics.h"
 #include "Player/POSTCharacter.h"
 #include "Player/POSTPlayerController.h"
 #include "POSTGameState.h"
@@ -9,8 +11,18 @@
 
 APOSTGameModeBase::APOSTGameModeBase()
 {
-	DefaultPawnClass = APOSTCharacter::StaticClass();
-	PlayerControllerClass = APOSTPlayerController::StaticClass();
-	GameStateClass = APOSTGameState::StaticClass();
-	HUDClass = APOSTHUD::StaticClass();
+    DefaultPawnClass = APOSTCharacter::StaticClass();
+    PlayerControllerClass = APOSTPlayerController::StaticClass();
+    GameStateClass = APOSTGameState::StaticClass();
+    HUDClass = APOSTHUD::StaticClass();
+}
+
+void APOSTGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (!UGameplayStatics::GetActorOfClass(this, APOSTCycleManager::StaticClass()))
+    {
+        GetWorld()->SpawnActor<APOSTCycleManager>(APOSTCycleManager::StaticClass());
+    }
 }
